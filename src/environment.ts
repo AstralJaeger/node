35d6183ea45fdb314be673c0080415abc2f349b3
Felix Hillebrand <97hilfel@gmail.com>
2022-08-05 14:29:20 +0200

    Add environment.ts

--- src/environment.ts
+++ src/environment.ts
@@ -1,19 +1,31 @@
-import env from "env-var";
+import env from 'env-var';
 
+/**
+ * Type safe environment variables read as singleton.
+ */
 export class Environment {
+  private static instance: Environment;
 
-    private static instance: Environment
+  /**
+   * Sentry DSL Token for Sentry Analytics
+   */
+  public readonly SENTRY_TOKEN: string;
 
-    public readonly SENTRY_TOKEN: string;
+  /**
+   * Private constructor, reads environment
+   * @private
+   */
+  private constructor() {
+    this.SENTRY_TOKEN = env.get('SENTRY_TOKEN').required().asString();
+  }
 
-    private constructor() {
-        this.SENTRY_TOKEN = env.get("SENTRY_TOKEN").required().asString();
+  /**
+   * @return Environment Returns the environment instance for later use.
+   */
+  public static getInstance(): Environment {
+    if (Environment.instance == null) {
+      Environment.instance = new Environment();
     }
-
-    public getInstance(): Environment {
-        if (Environment.instance == null) {
-            Environment.instance = new Environment();
-        }
-        return Environment.instance;
-    }
+    return Environment.instance;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log("Hello World");
+import {Environment} from './environment';
+
+const env = Environment.getInstance();
+console.log(`Sentry Token set: ${env.SENTRY_TOKEN != null}`);
