52dad09854bbb2d330ca947cbcb69113d6479074
Felix Hillebrand <97hilfel@gmail.com>
2022-08-08 08:33:25 +0200

    Update Index.ts, add build.yml, add dependabot.yml

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ import {Environment} from './environment';
 
 const env = Environment.getInstance();
 console.log(`Sentry Token set: ${env.SENTRY_TOKEN != null}`);
+
+process.on("SIGINT", () => {
+  // Graceful shutdown here
+  process.exit();
+});
